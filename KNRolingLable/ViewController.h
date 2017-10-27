446650ad442b1b95a104b2fd8cbe814c617dc6f2
Leessang <992275294@qq.com>
2017-10-27 13:53:35 +0800

    Initial Commit

---
+++ KNRolingLable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KNRolingLable
+//
+//  Created by 刘凡 on 2017/10/27.
+//  Copyright © 2017年 KrystalName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
